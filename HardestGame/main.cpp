cc756514b04c8489f3571bc0aea716eb357a29f1
Ajay Parrish <ajay.parrish@wsu.edu>
2025-04-18 20:58:08 -0700

    try to fix collision

--- HardestGame/main.cpp
+++ HardestGame/main.cpp
@@ -126,7 +126,7 @@ int main()
                     switch (levelTracker)
                     {
                     case 1:
-                        levelCheck = levelOne(window, background, LvlOneMsg);
+                        levelCheck = levelOne(window, gameBackground, LvlOneMsg);
                         if (levelCheck) 
                         {
                             levelTracker++;
